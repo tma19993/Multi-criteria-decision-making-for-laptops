21d55317081f2d52de1e35953c348036ba8a9320
Tomasz Mazurek <tmazzurek@gmail.com>
2022-04-13 21:32:06 +0200

    Dodanie loga

--- frontend/src/components/StartComponent.js
+++ frontend/src/components/StartComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "../styles/StartComponent.css";
+import logo from "../pictures/logo.png";
 class StartComponent extends Component {
   render() {
     return (
       <section id="StartSide">
         <div className="StartBGC"></div>
+        <img src={logo} alt="logo" className="logo" />
         <div className="welcome">
           <h1>Witaj</h1>
           <p>Ta aplikcaja pomoże ci wybrać swój wymarzony laptop</p>
